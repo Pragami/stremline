a6c5424ffdd10df47992e919912bfe5365ce747f
myworkat2025 <myworkat2025@gmail.com>
2025-02-22 17:03:22 +0530

    first commit

---
+++ models/taskModel.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    date: { type: Date, default: new Date() },
+    priority: {
+      type: String,
+      default: "normal",
+      enum: ["high", "medium", "normal", "low"],
+    },
+    stage: {
+      type: String,
+      default: "todo",
+      enum: ["todo", "in progress", "completed"],
+    },
+    isApproved: { type: Boolean, default: false }, // ✅ NEW: Admin approval for task completion
+
+    activities: [
+      {
+        type: {
+          type: String,
+          default: "assigned",
+          enum: [
+            "assigned",
+            "started",
+            "in progress",
+            "bug",
+            "completed",
+            "commented",
+            "progress update",
+          ],
+        },
+        activity: String,
+        date: { type: Date, default: new Date() },
+        by: { type: Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+    subTasks: [
+      {
+        title: String,
+        date: Date,
+        tag: String,
+      },
+    ],
+    assets: [String],
+    team: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    isTrashed: { type: Boolean, default: false },
+    department: { type: Schema.Types.ObjectId, ref: "Department" },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
