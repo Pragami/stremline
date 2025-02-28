8cf9808769cb0afb9c940f7667558951967622eb
Pragami <rakesh@pragami.com>
2025-02-28 22:32:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://pragami-streamline.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://pragami-streamline.netlify.app/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
